1ea25f1a5378c7733f59f8fb656c05436fcb5484
root <root@ip-172-31-41-120.us-east-2.compute.internal>
2018-06-05 14:58:32 +0000

    g

--- application/views/close.php
+++
@@ -1,7 +0,0 @@
-
-
-<script type="text/javascript">
-  $(document).ready(function(){
-    location.href = "<?=site_url()?>";
-  });
-</script>
--- try.php
+++
@@ -1,5 +0,0 @@
-<?php echo json_encode($_SERVER['REQUEST_SCHEME']) ?>
-
-<!-- REQUEST_SCHEME -->
-<!-- SERVER_NAME -->
-<!-- SERVER_PORT -->
